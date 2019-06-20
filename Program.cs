1ff56756bd9da8d21f243f695f7220e764365271
Sergey <47604847+SEREGA2976@users.noreply.github.com>
2019-06-21 00:20:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+namespace MailSenderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SmtpClient client = new SmtpClient("smtp yandex", 25);
+            // MailMessage msg = new MailMessage();
+            using (var client = new SmtpClient("smtp yandex", 25)
+            {
+                Credentials = new NetworkCredential("UserName", "Password"),
+                EnableSsl = true
+            })
+            {
+                using (var msg = new MailMessage
+                {
+                    From = new MailAddress("sender@yandex.ru", "Отправитель"),
+                    To =
+                {
+                    new MailAddress("recipient@gmail.com", "Получатель")
+
+                },
+                    Subject = "Тема письма",
+                    Body = "Тело письма",
+                    Attachments =
+                {
+                    new Attachment("FileName.zip")
+                }
+                })
+                {
+                    client.Send(msg);
+                    client.Dispose();
+                    msg.Dispose();
+                }
+            }
+
+        }
+
+    }
+}
